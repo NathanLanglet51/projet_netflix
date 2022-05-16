17eca53713663200f7490a3473099847f0b689b7
nathanlanglet <nathan-51@hotmail.fr>
2022-05-16 12:43:21 +0200

    sdf

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -9,10 +9,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class PagesController extends AbstractController
 {
     /**
-     * @Route("/pages", name="accueil")
+     * @Route("/", name="app_accueil")
      */
     public function accueil(): Response
     {
         return $this->render('pages/accueil.html.twig');
+    }
+
+    /**
+     * @Route("/a-propos", name="app_a-propos")
+     */
+    public function aPropos(): Response
+    {
+        return $this->render('pages/a-propos.html.twig');
+    }
+
+    
+    /**
+     * @Route("/liste-films", name="app_liste-films")
+     */
+    public function listFilms(): Response
+    {
+        return $this->render('pages/liste-films.html.twig');
     }
 }
